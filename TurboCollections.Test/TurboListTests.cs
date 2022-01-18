9ac294438a42401983cdb4335b19496c4b36f584
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2022-01-18 01:04:15 +0100

    Added RemoveAt() + Contains() + IndexOf()

--- TurboCollections.Test/TurboListTests.cs
+++ TurboCollections.Test/TurboListTests.cs
@@ -37,5 +37,41 @@ namespace TurboCollections.Test {
             list.Clear();
             Assert.AreEqual(0, list.Count);
         }
+
+        [Test]
+        public void RemoveOneSpecificItemAndMoveElementsToLeft() {
+            var list = new TurboList<int>();
+            list.Add(6);
+            list.Add(9);
+            list.Add(3);
+            list.Add(5);
+            list.Add(7);
+            list.Add(8);
+            list.RemoveAt(0);
+            Assert.AreEqual(9, list.Get(0));
+        }
+
+        [Test]
+        public void SeeIfItemExists() {
+            var list = new TurboList<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+            list.Add(7);
+            list.Add(9);
+            Assert.True(list.Contains(5));
+            Assert.False(list.Contains(12));
+        }
+
+        [Test]
+        public void ReturnIndexIndexOf() {
+            var list = new TurboList<int>();
+            list.Add(11);
+            list.Add(32);
+            list.Add(56);
+            list.Add(71);
+            list.Add(90);
+            Assert.AreEqual(2, list.IndexOf(56));
+        }
     }
 }
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,7 +1,9 @@
 ﻿namespace TurboCollections {
-    public class TurboList<T> {
+    public class TurboList<T>
+    {
         public int Count => items.Length;
         T[] items = Array.Empty<T>();
+
         public TurboList() {
             Console.WriteLine("Hello, Turbo!");
         }
@@ -27,6 +29,39 @@
 
         public void Clear() {
             items = Array.Empty<T>();
+        }
+
+        public void RemoveAt(int index) {
+            T[] newArray = new T[Count - 1];
+            for (int i = 0; i < Count - 1; i++) {
+                if (i >= index) {
+                    newArray[i] = items[i + 1];
+                } else {
+                    newArray[i] = items[i];
+                }
+            }
+            items = newArray;
+            Console.WriteLine(string.Join(",", newArray));
+        }
+
+        public bool Contains(T item) {
+            foreach (var elements in items) {
+                if (elements.Equals(item)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(T item) {
+            int i = 0;
+            foreach (var elements in items) {
+                if (elements.Equals(item)) {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
         }
     }
 }
